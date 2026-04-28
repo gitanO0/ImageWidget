e2305ae86479ba3e3820ec9dd44141362fa2ca9e
royce_s <roycenlisa@gmail.com>
2026-04-28 00:01:50 -0600

    V1.24 Auto apk builds and release page actions
    Signed-off-by: royce_s <roycenlisa@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.royce.imagewidget"
         minSdk = 24
         versionCode = 3
-        versionName = "1.23"
+        versionName = "1.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
